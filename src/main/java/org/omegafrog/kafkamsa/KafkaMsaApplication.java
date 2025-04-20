2cb5458a8ff540bbe9c3ee22b124977192949058
omegafrog <jiwoo22110103@gmail.com>
2025-04-20 11:15:46 +0900

    init

---
+++ src/main/java/org/omegafrog/kafkamsa/KafkaMsaApplication.java
@@ -0,0 +1,13 @@
+package org.omegafrog.kafkamsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMsaApplication.class, args);
+	}
+
+}

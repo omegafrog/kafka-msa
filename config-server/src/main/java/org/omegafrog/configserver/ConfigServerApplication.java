21a9a1a8736a940d534970ab1aa128c889ca9f04
omegafrog <jiwoo22110103@gmail.com>
2025-04-20 12:53:09 +0900

    add config file loading configure yml and test

--- config-server/src/main/java/org/omegafrog/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/org/omegafrog/configserver/ConfigServerApplication.java
@@ -2,8 +2,9 @@ package org.omegafrog.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
---
+++ order-service/src/main/java/org/omegafrog/orderservice/config/ConfigController.java
@@ -0,0 +1,16 @@
+package org.omegafrog.orderservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+	@Value("${server.port}")
+	private String port;
+	@GetMapping("/healthCheck")
+	public String hi(){
+		return port;
+	}
+}
